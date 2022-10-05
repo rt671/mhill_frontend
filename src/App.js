171f932ca8bd1951b9c4bf43b3ceae122027e7e6
Rudraksh <rudrakshsuper2001@gmail.com>
2022-10-05 15:37:38 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './app.css'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Main from './Components/Main/Main'
 import NavBar from './Components/NavBar/NavBar'
 import Home from './Components/Home/Home'
@@ -7,13 +8,17 @@ import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
-    <>
-    <NavBar/>
-    <Home/>
-    <Main/>
-    <Footer/>
-    </>
-  )
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route exact path="/" element={<><Home/><Main/></>}/>
+
+        {/* <Route path="/trips/:id" element={<Trip/>}/>
+        */}
+      </Routes>
+      <Footer/>
+    </Router>
+  );
 }
 
 export default App
