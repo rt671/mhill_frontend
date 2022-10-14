c5a41c19b8d7a159e3a64f4bf0a58b9296028e7b
Rudraksh <rudrakshsuper2001@gmail.com>
2022-10-15 00:58:24 +0530

    Separated Card Component

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,11 +3,7 @@ import "./main.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import axios from "axios";
-import {
-  HiOutlineClipboardCheck,
-  HiOutlineLocationMarker,
-} from "react-icons/hi";
-import { Link } from "react-router-dom";
+import Card from "../Card/Card";
 
 const Main = () => {
   const [trips, setTrips] = useState([]);
@@ -27,59 +23,10 @@ const Main = () => {
 
   return (
     <section className="main container section">
-      <div className="secTitle">
         <h3 data-aos="fade-right" className="title">
           Most Visited Destinations
         </h3>
-      </div>
-
-      <div className="secContent grid">
-        {trips.map((trip) => {
-          return (
-            <div
-              key={trip._id}
-              data-aos="fade-up"
-              className="singleDestination"
-            >
-              {trip.photo && (
-                <div className="imageDiv">
-                  <img src={trip.photo} alt={trip.destination} />
-                </div>
-              )}
-              <div className="cardInfo">
-                <div className="titles">
-                  <div className="tseason">
-                    <span className="destTitle">{trip.destination}</span>
-                    <span className="destSeason">{trip.seasons}</span>
-                  </div>
-                  <span className="continent flex">
-                    <HiOutlineLocationMarker className="icon" />
-                    <span className="name">{trip.state}</span>
-                  </span>
-                </div>
-
-                <div className="flex fees">
-                  <div className="duration">
-                    <h5>{trip.duration}</h5>
-                  </div>
-                  <div className="price">
-                    <h5>₹{trip.price}</h5>
-                  </div>
-                </div>
-
-                <div className="desc">
-                  <p>{trip.desc}</p>
-                  <Link to={`/trips/${trip._id}`} className="link">
-                    <button className="btn flex">
-                      DETAILS <HiOutlineClipboardCheck className="icon" />
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <Card trips={trips}/>
     </section>
   );
 };
