22adff9b30cd0a352a8daf1a9bb356f818c0f523
Vaibhav-Fulara <vaibhav.fulara123@gmail.com>
2022-10-14 22:37:59 +0530

    added sass compiler

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,75 +1,83 @@
-import React from "react";
+import React from "react"; 
 import { Container, Row, Col } from "react-bootstrap";
-import "./Contact.css"
+import { contactConfig } from "./content_option"
+import './Contact.css'
 
-const contactUs = () => {
-    return (
-        <Container>
-            <Row className="mb-5 mt-3">
-                <Col lg='8'>
-                    <h1 className="display-4 mb-4">
-                        Contact Us!
-                    </h1>
-                </Col>
-            </Row>
+export default function ContactUs() {
+  
 
-            <Row className="sec_sp">
-                <Col lg='5' className="mb-5">
-                    <h3 className="color_sec py-4">Get in touch</h3>
-                    <address>
-                        <strong>Email: mhilladventure@gmail.com</strong>
-                        <br />
-                        <br />
-                        <p>
-                            <strong>Phone: +91 94129 66171</strong>
-                            <br />
-                            <strong>Phone: +91 63968 89138</strong>
-                            <br />
-                            <strong>Phone: +91 91197 60282</strong>
-                        </p>
-                    </address>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eos sed, a sint soluta quidem ipsam quis eligendi temporibus aspernatur aut veritatis consequatur recusandae voluptatem culpa libero aliquam omnis provident excepturi fugiat enim asperiores quaerat ab. Atque vel maxime corrupti tenetur sapiente, amet quod quam et tempora deleniti odit in, laudantium, aspernatur repudiandae? Magni officia aut, harum sapiente reiciendis vitae, numquam, soluta accusantium delectus inventore laborum quas iure? Dolorem unde dolore maiores esse nobis, consectetur laborum eum quibusdam odio vero soluta illo eaque magni amet explicabo? Eius aperiam accusantium accusamus sed nihil praesentium mollitia impedit. Quam ad vel quia dicta.</p>
+  return (
+      <Container>
+     
+        <Row className="mb-5 mt-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4">Contact Me</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="5" className="mb-5">
+            <h3 className="color_sec py-4">Get in touch</h3>
+            <address>
+              <strong>Email:</strong>{" "}
+              <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
+                {contactConfig.YOUR_EMAIL}
+              </a>
+              <br />
+              <br />
+              {contactConfig.hasOwnProperty("YOUR_FONE") ? (
+                <p>
+                  <strong>Phone:</strong> {contactConfig.YOUR_FONE}
+                </p>
+              ) : (
+                ""
+              )}
+            </address>
+            <p>{contactConfig.description}</p>
+          </Col>
+          <Col lg="7" className="d-flex align-items-center">
+            <form  className="contact__form w-100">
+              <Row>
+                <Col lg="6" className="form-group">
+                  <input
+                    className="form-control"
+                    id="name"
+                    name="name"
+                    placeholder="Name" 
+                    type="text"
+                    required 
+                  />
                 </Col>
-                <Col lg='7' className="d-flex align-items-center">
-                    <form className="contact_form w-100">
-                        <Row>
-                            <Col lg='6' className="form-group">
-                                <input 
-                                    className="form-control"
-                                    id="name"
-                                    name="name"
-                                    placeholder="Name"
-                                    type="text"
-                                />
-                            </Col>
-                            <Col lg='6' className="form-group">
-                                <input 
-                                    className="form-control rounded-0"
-                                    id="email"
-                                    name="email"
-                                    placeholder="Email"
-                                    type="email"
-                                />
-                            </Col>
-                        </Row>
-                        <textarea
-                            className="form_control rounded-0"
-                            id="message"
-                            name="message"
-                            placeholder="Message"
-                            rows="5"
-                        ></textarea>
-                        <br />
-                        <Row>
-                            <Col lg='12' className="form-group">
-                                <button className="btn ac_btn" type="submit">Send</button>
-                            </Col>
-                        </Row>
-                    </form>
+                <Col lg="6" className="form-group">
+                  <input
+                    className="form-control rounded-0"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    type="email" 
+                    required 
+                  />
                 </Col>
-            </Row>
-        </Container>
-    );
+              </Row>
+              <textarea
+                className="form-control rounded-0"
+                id="message"
+                name="message"
+                placeholder="Message"
+                rows="5" 
+                required
+              ></textarea>
+              <br />
+              <Row>
+                <Col lg="12" className="form-group">
+                  <button className="btn ac_btn" type="submit"> 
+                  Send
+                  </button>
+                </Col>
+              </Row>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+  );
 }
-
-export default contactUs;
