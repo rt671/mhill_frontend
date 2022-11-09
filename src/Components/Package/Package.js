dda76f279589b879e9562572e37638d1d195dcf2
Rudraksh <rudrakshsuper2001@gmail.com>
2022-11-09 23:44:17 +0530

    idk

--- src/Components/Package/Package.js
+++ src/Components/Package/Package.js
@@ -11,7 +11,7 @@ const Package = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/trips/seasons/" + path)
+      .get("http://mhill-api.herokuapp.com/trips/" + path)
       .then((res) => {
         console.log(res.data);
         setTrips(res.data);
